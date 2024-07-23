968726d768cfd3cea1542942e4394f926b574c38
Your Name <you@example.com>
2024-07-24 04:42:44 +0700

    update

--- ls3/src/Modal/ModalPokemon.jsx
+++ ls3/src/Modal/ModalPokemon.jsx
@@ -6,8 +6,10 @@ import { faCircleXmark, faHouseMedicalCircleExclamation } from '@fortawesome/fre
 import { card } from '../components(rfce)/cardPokemon/dataCard';
 
 
-function ModalPokemon({ itemCard }) {
-
+function ModalPokemon({...its}) {
+    
+    let items = its.itemCard
+    console.log(items)
     const closeModal = () => {
         const modalPokemon = document.getElementById('modalPokemon')
         modalPokemon.classList.remove('show')
@@ -16,10 +18,17 @@ function ModalPokemon({ itemCard }) {
         for (let i = 0; i < inpText.length; i++) {
             inpText[i].value = ''
         }
-
     }
 
-
+    
+    let itemCard = {}
+    let itemObject = {}
+    if(items !== null && items !== undefined ){
+    const cardObject = JSON.parse(localStorage.getItem('userCard'))
+    itemObject = Object.values(cardObject).find(itm => itm.name === items.name )
+    itemCard = {...itemObject}
+    }
+    
 
 
     var atbNoneNull = []
@@ -33,21 +42,47 @@ function ModalPokemon({ itemCard }) {
         }
 
     }
-    const updateAttribute = (e) => {
+    
+    const getParam = (e) => {
         const namePokemon = e.target.attributes[0].value
         const attributePokemon = e.target.attributes[2].value
         const newAttributePokemon = e.target.value
+
+        document.getElementsByClassName('button-update').id = `${namePokemon}${attributePokemon}`
+        document.getElementsByClassName('button-update').content = `${newAttributePokemon}`
+        document.getElementsByClassName('button-update').namePokemon = `${namePokemon}`
+        document.getElementsByClassName('button-update').attribute = `${attributePokemon}`
+    }
+
+
+
+    const updateAtribute = (e) => {
+        const idSpan = document.getElementsByClassName('button-update').id
+        const newAttri = document.getElementsByClassName('button-update').content
+        const namePokemon = document.getElementsByClassName('button-update').namePokemon
+        const oldAttri = document.getElementsByClassName('button-update').attribute
+
         for (let item of card) {
             if (item.name === namePokemon) {
                 let arrKey = Object.keys(item)
-                let keyNeedUpdate = arrKey.find(key => item[key] === attributePokemon)
+                let keyNeedUpdate = arrKey.find(key => item[key] === oldAttri)
                 if (keyNeedUpdate !== undefined) {
-                    item[keyNeedUpdate] = newAttributePokemon
+                    item[keyNeedUpdate] = newAttri
+
                 }
             }
         }
 
+
+        document.getElementById(idSpan).innerText = newAttri
+        document.getElementById(idSpan).setAttribute('class', `App-${newAttri}`)
+
+        closeModal()
+        const cardPresent = { ...card }
+        localStorage.setItem('userCard', JSON.stringify(cardPresent))
+
     }
+   
     return (
         <div id='modalPokemon' className='' >
 
@@ -57,17 +92,27 @@ function ModalPokemon({ itemCard }) {
                     <img className="modal-img" src={itemCard.img} alt={itemCard.name} />
                     <FontAwesomeIcon icon={faCircleXmark} size="lg" style={{ color: "#63E6BE", }} onClick={closeModal} />
                 </div>
-                <button className='button-update' >Cập nhật thuộc tính</button>
+
                 <div className='background-information'>
-                    <section className='inf-name'>{itemCard.number}</section>
-                    <section className='inf-name'>{itemCard.name}</section>
+                    <section className='inf-name'>
+                        {itemCard.number}
+                        <button
+                            className='button-update'
+                            id=''
+                            content=''
+                            namePokemon=''
+                            attribute=''
+                            onClick={updateAtribute}  >Cập nhật thuộc tính</button>
+                    </section>
+                    <section className='inf-name'>
+                        {itemCard.name}
+                    </section>
                     {atbNoneNull.map((item) => (
-                        <input id={itemCard.name} className={`App-${item}`} placeholder={item}
-                            onChange={(e) => updateAttribute(e)} />
+                        <input id={itemCard.name} className={`App-${item}`} placeholder={item} onChange={getParam} />
                     ))}
                 </div>
             </div>
-            
+
         </div>
     )
 }
--- ls3/src/components(rfce)/cardPokemon/CardPerform.jsx
+++ ls3/src/components(rfce)/cardPokemon/CardPerform.jsx
@@ -2,67 +2,102 @@ import React, { useState } from 'react'
 import './card.css'
 import ModalPokemon from '../../Modal/ModalPokemon'
 // import { card } from './dataCard'
-import img001 from "../../images/001.png";
-import img002 from "../../images/002.png";
-import img003 from "../../images/003.png";
-import img004 from "../../images/004.png";
-import img005 from "../../images/005.png";
-import img006 from "../../images/006.png";
-import img007 from "../../images/007.png";
-import img008 from "../../images/008.png";
-import img009 from "../../images/009.png";
-import img010 from "../../images/010.png";
-import img011 from "../../images/011.png";
-import img012 from "../../images/012.png";
+// import img001 from "../../images/001.png";
+// import img002 from "../../images/002.png";
+// import img003 from "../../images/003.png";
+// import img004 from "../../images/004.png";
+// import img005 from "../../images/005.png";
+// import img006 from "../../images/006.png";
+// import img007 from "../../images/007.png";
+// import img008 from "../../images/008.png";
+// import img009 from "../../images/009.png";
+// import img010 from "../../images/010.png";
+// import img011 from "../../images/011.png";
+// import img012 from "../../images/012.png";
 
-const card = [
-  { name: 'Bulbasaur', number: '#0001', img: img001, attribute1: 'Grass', attribute2: 'Poison' },
-  { name: 'Ivysaur', number: '#0002', img: img002, attribute1: 'Grass', attribute2: 'Poison' },
-  { name: 'Venusaur', number: '#0003', img: img003, attribute1: 'Grass', attribute2: 'Poison' },
-  { name: 'Charmander', number: '#0004', img: img004, attribute1: 'Fire', attribute2: '' },
-  { name: 'Charmeleon', number: '#0005', img: img005, attribute1: 'Fire', attribute2: '' },
-  { name: 'Charizard', number: '#0006', img: img006, attribute1: 'Fire', attribute2: 'Flying' },
-  { name: 'Squirtile', number: '#0007', img: img007, attribute1: 'Water', attribute2: '' },
-  { name: 'Wartortle', number: '#0008', img: img008, attribute1: 'Water', attribute2: '' },
-  { name: 'Blastoise', number: '#0009', img: img009, attribute1: 'Water', attribute2: '' },
-  { name: 'Caterpie', number: '#0010', img: img010, attribute1: 'Bug', attribute2: '' },
-  { name: 'Metapod', number: '#0011', img: img011, attribute1: 'Bug', attribute2: '' },
-  { name: 'Butterfree', number: '#0012', img: img012, attribute1: 'Bug', attribute2: 'Flying' }
+const cardPK = [
+  { name: 'Bulbasaur', number: '#0001', img: `https://github.com/HybridShivam/Pokemon/blob/master/assets/images/001.png?raw=true`, attribute1: 'Grass', attribute2: 'Poison' },
+  { name: 'Ivysaur', number: '#0002', img: 'https://github.com/HybridShivam/Pokemon/blob/master/assets/images/002.png?raw=true', attribute1: 'Grass', attribute2: 'Poison' },
+  { name: 'Venusaur', number: '#0003', img: 'https://github.com/HybridShivam/Pokemon/blob/master/assets/images/003.png?raw=true', attribute1: 'Grass', attribute2: 'Poison' },
+  { name: 'Charmander', number: '#0004', img: 'https://github.com/HybridShivam/Pokemon/blob/master/assets/images/004.png?raw=true', attribute1: 'Fire', attribute2: '' },
+  { name: 'Charmeleon', number: '#0005', img: 'https://github.com/HybridShivam/Pokemon/blob/master/assets/images/005.png?raw=true', attribute1: 'Fire', attribute2: '' },
+  { name: 'Charizard', number: '#0006', img: 'https://github.com/HybridShivam/Pokemon/blob/master/assets/images/006.png?raw=true', attribute1: 'Fire', attribute2: 'Flying' },
+  { name: 'Squirtile', number: '#0007', img: 'https://github.com/HybridShivam/Pokemon/blob/master/assets/images/007.png?raw=true', attribute1: 'Water', attribute2: '' },
+  { name: 'Wartortle', number: '#0008', img: 'https://github.com/HybridShivam/Pokemon/blob/master/assets/images/008.png?raw=true', attribute1: 'Water', attribute2: '' },
+  { name: 'Blastoise', number: '#0009', img: 'https://github.com/HybridShivam/Pokemon/blob/master/assets/images/009.png?raw=true', attribute1: 'Water', attribute2: '' },
+  { name: 'Caterpie', number: '#0010', img: 'https://github.com/HybridShivam/Pokemon/blob/master/assets/images/010.png?raw=true', attribute1: 'Bug', attribute2: '' },
+  { name: 'Metapod', number: '#0011', img: 'https://github.com/HybridShivam/Pokemon/blob/master/assets/images/011.png?raw=true', attribute1: 'Bug', attribute2: '' },
+  { name: 'Butterfree', number: '#0012', img: 'https://github.com/HybridShivam/Pokemon/blob/master/assets/images/012.png?raw=true', attribute1: 'Bug', attribute2: 'Flying' }
 ]
 
 
 function CardPerform() {
-  const [showModalPK, setShowModalPK] = useState(false);
+  // Khởi tạo local strorage
+  if (localStorage.getItem('userCard') === null) {
+    localStorage.setItem('userCard', JSON.stringify(cardPK))
+  }
+  const cardObject = JSON.parse(localStorage.getItem('userCard'))
+  var cardArray = Object.values(cardObject).map(item => ({
+    name: item.name,
+    number: item.number,
+    img: item.img,
+    attribute1: item.attribute1,
+    attribute2: item.attribute2
+  }))
+  const [card, setCard] = useState(cardArray)
   const [currentItem, setItem] = useState(null)
-
+  // open Modal
   const openModal = (item) => {
-    setShowModalPK(true)
     const modalPokemon = document.getElementById('modalPokemon')
     modalPokemon.classList.add('show')
-    setItem(item)
-  
+    const cardObject = JSON.parse(localStorage.getItem('userCard'))
+    const cardArray = Object.values(cardObject).map(item => ({
+      name: item.name,
+      number: item.number,
+      img: item.img,
+      attribute1: item.attribute1,
+      attribute2: item.attribute2
+    }))
+    const newItem = cardArray.find(idx => idx.name === item.name)
+    setItem(newItem)
+    setCard(cardArray)
   }
 
 
-  return (
-    <div className="App-container">
 
-      {card.map((items,index)=>(
-        <div key={index} className="App-item">
-        <div className="background-color">
-          <img className="App-img" src={items.img} alt={items.name} onClick={()=>openModal(items)} />
-        </div>
-        <div>
-          <section>{items.number}</section>
-          <section className='App-name'>{items.name}</section>
-          <span className={`App-${items.attribute1}`}>{items.attribute1}</span>
-          <span className={`App-${items.attribute2}`}>{items.attribute2}</span>
-        </div>
-    
+  
+  const searchPekemon = (e) => {
+    const cardArrayFilter = cardArray.filter(item => item.name.toLowerCase().includes(e.target.value.toLowerCase()))
+    return setCard([...cardArrayFilter])
+  }
+
+
+
+  return (
+    <>
+      <search>
+        <label>Tìm kiếm Pokémon</label>
+        <input type='text' onChange={searchPekemon}></input>
+      </search>
+      <div className="App-container">
+
+        {card.map((items) => (
+          <div key={items.name} className="App-item">
+            <div className="background-color">
+              <img className="App-img" src={items.img} alt={items.name} onClick={() => openModal(items)} />
+            </div>
+            <div>
+              <section>{items.number}</section>
+              <section className='App-name'>{items.name}</section>
+              <span className={`App-${items.attribute1}`} id={`${items.name}${items.attribute1}`}>{items.attribute1}</span>
+              <span className={`App-${items.attribute2}`} id={`${items.name}${items.attribute2}`}>{items.attribute2}</span>
+            </div>
+
+          </div>
+        ))}
+        <ModalPokemon itemCard={currentItem} />
       </div>
-    ))}
-        <ModalPokemon itemCard = {currentItem} />
-    </div>
+    </>
   )
 }
 
