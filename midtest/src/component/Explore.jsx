b383d7400638616135f2194793b407ed65a07a87
tivih1996 <tivih1996@gmail.com>
2024-07-28 03:51:33 +0700

    update

--- midtest/src/component/Explore.jsx
+++ midtest/src/component/Explore.jsx
@@ -1,38 +1,52 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import { dataMovie } from '../data/dataMovie'
 import './StyleExplore.css'
 
 function Explore() {
-    // hiệu ứng chuyển động slide show
+  // hiệu ứng chuyển động slide show
+  const [idxImg, setidxImg] = useState(0)
+  const lengthImg = document.getElementsByClassName('img-movie-explore').length
+  const listMovieScroll = document.querySelector('.list-movie-scroll')
+
+  console.log(idxImg)
+  useEffect(() => {
+    const intervalid = setInterval(() => {
       const listMovieScroll = document.querySelector('.list-movie-scroll')
-      // const withImg = document.getElementsByTagName('img')[0].offsetWidth
-      // console.log(withImg)
-      // setInterval(() => {
-      //   listMovieScroll.style.transform = `translateX(${withImg * -1}px)`
-      // }, 2000)
-    // ------
+      listMovieScroll.style.transform = `translateX(${1170 * -1 * idxImg}px)`
+      setidxImg(previdxImg => (previdxImg +1) % 7)
+    }, 4000)
+
+    return () => clearInterval(intervalid)
+  }, [idxImg])
+  // console.log(withImg)
+  // setInterval(() => {
+  //   listMovieScroll.style.transform = `translateX(${withImg * -1}px)`
+  // }, 2000)
+  // ------
 
 
   return (
-  <>
-    <div className='explore'>
-    <h1> Explore </h1>
+    <>
+      <div className='explore'>
+        <h1> Explore </h1>
 
-    <p>What are you gonna watch today ?</p>
-
-    <div className='list-movie-scroll'>
-      {dataMovie.map(item =>(
-        <section className='item-move-scroll'>
-            <img src={item.poster} className='img-movie-explore' />
-            <div className='content-explore'>
-            <p className='name-movie'>{item.movieName}</p>
-            <p className='description-movie'>{item.description}</p>
-            </div>
-        </section>
-      ))}
-    </div>
-    </div>
-</>
+        <p>What are you gonna watch today ?</p>
+        <div className='slide-show-movie'>
+          <div className='list-movie-scroll'>
+            {dataMovie.map(item => (
+              <section className='item-movie-scroll'>
+                <img src={item.poster} className='img-movie-explore' />
+                <div className='content-explore'>
+                  <p className='name-movie'>{item.movieName}</p>
+                  <p className='description-movie'>{item.description}</p>
+                </div>
+              </section>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- midtest/src/component/Home.jsx
+++ midtest/src/component/Home.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import NewRealease from './NewRealease'
 import Explore from './Explore'
+import { Link } from 'react-router-dom'
 import './StyleHome.css'
 function Home() {
   return (
     <>
-    
-    <header className='header-home'>
-        <span></span>
-        <link></link>
-        <link></link>
-        <input type='text' ></input>
-    </header>
-    <main className='main-home'>
-    <Explore  />
-    <NewRealease  />
-    </main>
+
+      <header className='header-home'>
+        <div className='navbar'>
+          <span className='anonime'> Anonime</span>
+          <span className='home'> Home</span>
+          <span className='listanime'> List anime</span>
+          <input className='input-name-movie' type='text' placeholder='Search anime or movie' ></input>
+        </div>
+      </header>
+      <main className='main-home'>
+        <Explore />
+        <NewRealease />
+      </main>
     </>
   )
 }
